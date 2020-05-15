0c985ffd99b388d9cf6951b0d3df713980e60dad
sergeyDanila <sergey.danila@gmail.com>
2020-05-15 13:29:49 +0300

    ready +all

--- filter.py
+++
@@ -1,8 +0,0 @@
-def minmaxstring(lst, s):
-    min_v = max_v = lst[0]
-    for i in lst:
-        if i < min_v:
-            min_v = i
-        if i > max_v:
-            max_v = i
-    return (s.replace('#min#', str(min_v))).replace('#max#', str(max_v))
